c55ffe69368facc05214392133c1d456f72be0fc
sushi009 <sushant.mithila@gmail.com>
2025-02-03 22:03:55 +0000

    Deploying to gh-pages from @ sushi009/polars@cc7cc4de37c218691dac8ed975ab7757781b0fa5 🚀

---
+++ api/rust/dev/polars_core/error/sidebar-items.js
@@ -0,0 +1,1 @@
+window.SIDEBAR_ITEMS = {"enum":["PolarsError","PolarsWarning"],"fn":["check_signals","get_warning_function","map_err","set_signals_function","set_warning_function","to_compute_err"],"macro":["feature_gated","polars_bail","polars_ensure","polars_err","polars_warn"],"mod":["constants"],"struct":["ErrString"],"type":["PolarsResult"]};
---
+++ api/rust/dev/polars_io/catalog/unity/models/sidebar-items.js
@@ -0,0 +1,1 @@
+window.SIDEBAR_ITEMS = {"enum":["DataSourceFormat","TableType"],"struct":["CatalogInfo","ColumnInfo","SchemaInfo","TableInfo"]};
